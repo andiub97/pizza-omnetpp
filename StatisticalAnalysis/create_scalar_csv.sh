93011659c804d15f5bdce0667d1dbc57cb22b11b
Matteo Sanfelici <sanfelicimatteo@gmail.com>
2020-01-13 11:01:44 +0100

    Refactor repository

--- StatisticalAnalysis/create_scalar_csv.sh
+++ StatisticalAnalysis/create_scalar_csv.sh
@@ -1,10 +1,13 @@
-for data in "results" "results_no_transient"
+mkdir -p "data"
+for data in "results" "results_no_transient" "single-results" "single-results_no_transient"
 do
+  mkdir -p data/data_${data}
   cd ../${data}
   for config in "First" "Second" "Third"
   do
     for n in 2 3 4 5
     do
+      mkdir -p ../StatisticalAnalysis/data/data_${data}/${config}-n${n}
       scavetool x -f 'module("*.EntryQueueU1") AND (name("queueLength:mean"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/EntryQueueLengthMeanU1.csv -F CSV-S ${config}-n${n}-*.sca
       scavetool x -f 'module("*.EntryQueueU2") AND (name("queueLength:mean"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/EntryQueueLengthMeanU2.csv -F CSV-S ${config}-n${n}-*.sca
       scavetool x -f 'module("*.sink") AND (name("lifeTime:mean"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/LifeTimeMeanTotal.csv -F CSV-S ${config}-n${n}-*.sca
@@ -16,6 +19,8 @@ do
       scavetool x -f 'module("*.sink") AND (name("lifeTime:max"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/LifeTimeMaxTotal.csv -F CSV-S ${config}-n${n}-*.sca
       scavetool x -f 'module("*.sink") AND (name("lifeTimeU1:max"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/LifeTimeMaxU1.csv -F CSV-S ${config}-n${n}-*.sca
       scavetool x -f 'module("*.sink") AND (name("lifeTimeU2:max"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/LifeTimeMaxU2.csv -F CSV-S ${config}-n${n}-*.sca
+      scavetool x -f 'module("*.CS") AND (name("timeInsideSystem:mean"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/timeInsideSystemMean.csv -F CSV-S ${config}-n${n}-*.sca
+      scavetool x -f 'module("*.CS") AND (name("timeInsideSystem:max"))' -o ../StatisticalAnalysis/data/data_${data}/${config}-n${n}/timeInsideSystemMax.csv -F CSV-S ${config}-n${n}-*.sca
     done
   done
 done
--- StatisticalAnalysis/create_vectorial_json.sh
+++ StatisticalAnalysis/create_vectorial_json.sh
@@ -1,4 +1,4 @@
-for results in "results" "results_no_transient"
+for results in "results" "results_no_transient" "single-results" "single-results_no_transient"
 do
 cd ../${results}
 echo ${results}
@@ -6,6 +6,11 @@ echo ${results}
   do
     for n in 2 3 4 5
     do
+      mkdir -p ../StatisticalAnalysis/data_vectorial/data_${results}_json/lifeTime
+      mkdir -p ../StatisticalAnalysis/data_vectorial/data_${results}_json/lifeTimeU1
+      mkdir -p ../StatisticalAnalysis/data_vectorial/data_${results}_json/lifeTimeU2
+      mkdir -p ../StatisticalAnalysis/data_vectorial/data_${results}_json/queueLengthU1
+      mkdir -p ../StatisticalAnalysis/data_vectorial/data_${results}_json/queueLengthU2
       echo ${config}-n${n}
       scavetool x -f 'module("Pizza.sink") AND (name("lifeTime:vector"))' -o ../StatisticalAnalysis/data_vectorial/data_${results}_json/lifeTime/${config}-n${n}.json -F JSON ${config}-n${n}-*.vec
       scavetool x -f 'module("Pizza.sink") AND (name("lifeTimeU1:vector"))' -o ../StatisticalAnalysis/data_vectorial/data_${results}_json/lifeTimeU1/${config}-n${n}.json -F JSON ${config}-n${n}-*.vec
